d756aa96dafa712eac8623ea31c6e951d0d32f69
Kleamens <maksymzirchuk@gmail.com>
2025-04-18 12:01:01 +0200

    feat: spock dependencies + base structure
    Since in most cases you don't need several unit testing frameworks, the part with the JUnit tests is commented

---
+++ moviesAPI/src/test/groovy/cz/mendelu/ea/xzirchuk/project/moviesAPI/director/DirectorUnitSpecification.groovy
@@ -0,0 +1,13 @@
+package cz.mendelu.ea.xzirchuk.project.moviesAPI.director
+
+import spock.lang.Specification
+
+class DirectorUnitSpecification extends  Specification {
+
+    def "testing"(){
+        expect:
+            1==2
+
+    }
+
+}
